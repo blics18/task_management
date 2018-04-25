5b8044b2b007a8672d052be653e20218d64bab36
BL MacBook <BLMac@Brendas-MacBook-Pro.local>
2018-04-24 23:29:11 -0700

    Checked email and password for /login

--- routes/auth.js
+++ routes/auth.js
@@ -8,12 +8,12 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/register', function(req, res){
-  db.sequelize.query('INSERT INTO "taskUsers" ("firstName", "lastName", email, password) VALUES ( :firstName, :lastName, :email, :password )', {replacements: { firstName: req.body.firstName.trim(), lastName: req.body.lastName.trim(), email: req.body.email.trim(), password: req.body.password.trim()}, type: db.sequelize.QueryTypes.INSERT, returning: true, plain: true})
+  db.sequelize.query('INSERT INTO "taskUsers" ("firstName", "lastName", email, password) VALUES ( :firstName, :lastName, :email, :password )', {replacements: { firstName: req.body.firstName.trim(), lastName: req.body.lastName.trim(), email: req.body.email.trim(), password: req.body.password.trim()}, type: db.sequelize.QueryTypes.INSERT})
   .then(function(result){
     if (result[1]){
-      db.sequelize.query('SELECT * FROM "taskUsers" WHERE email=:email', {replacements: {email: req.body.email.trim()}, type: db.sequelize.QueryTypes.SELECT, returning: true, plain: true})
+      db.sequelize.query('SELECT * FROM "taskUsers" WHERE email=:email', {replacements: {email: req.body.email.trim()}, type: db.sequelize.QueryTypes.SELECT})
       .then(sessionUser => {
-        console.log("USER ", sessionUser);
+        console.log("/register user ", sessionUser);
         req.session.user = sessionUser;
         res.status(200).redirect('/home');
       })
@@ -24,17 +24,21 @@ router.post('/register', function(req, res){
     }
   })
   .catch(function(err){
-    res.status(500).send(err);
+    res.status(500).render('auth', {authRegError: 'Email already in use'});
     return console.error(err);
   });
 });
 
 router.post('/login', function(req, res){
-  // sequelize.query("SELECT * FROM task_user WHERE task_user.email=" + req.body.email.trim() + " AND task_user.password=" + req.body.password.trim(), {type: sequelize.QueryTypes.SELECT})
-  db.sequelize.query("SELECT * FROM taskUsers", {type: db.sequelize.QueryTypes.SELECT})
+  db.sequelize.query('SELECT * FROM "taskUsers" WHERE email=:email AND password=:password', {replacements: {email: req.body.email.trim(), password: req.body.password.trim()}, type: db.sequelize.QueryTypes.SELECT})
   .then(function(user){
-    console.log("USER", user);
-    res.status(200).redirect('/home');
+    if (user.length != 0){
+      console.log("/login User ", user);
+      req.session.user = user;
+      res.status(200).redirect('/home');
+    }else{
+      res.render('auth', {authError: 'Incorrect Username or Password'});
+    }
   })
   .catch(function(err){
     res.status(500).send(err);
